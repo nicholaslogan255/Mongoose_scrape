6b6c9dbb4c84b19980e6747068b1f0a29485d1a9
Nic_Logan <nicholaslogan255@gmail.com>
2018-08-27 14:27:53 -0500

    Fix 5

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get("/articles", function (req, res) {
       console.log("Articles: " + dbArticle);
 
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.send(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
